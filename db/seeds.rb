91346789a577141929e9ea8ba334b446f3c616c6
Yuki Mizuno <muscular.mizuno@gmail.com>
2024-01-29 13:02:38 +0900

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,52 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user_data = {
-  email: 'user@example.com',
-  password: 'aaaaaaaa',
-  profile: {
-    name: 'テスト太郎',
-    gender: 'male'
-  }
-}
+user_data = [
+  {
+    email: 'user@example.com',
+    password: 'aaaaaaaa',
+    profile: {
+      name: 'テスト太郎',
+      gender: 'male'
+    }
+  },
+  {
+    email: 'user1@example.com',
+    password: 'aaaaaaaa',
+    profile: {
+      name: 'テスト太郎',
+      gender: 'male'
+    }
+  },
+  {
+    email: 'user2@example.com',
+    password: 'aaaaaaaa',
+    profile: {
+      name: 'テスト太郎',
+      gender: 'male'
+    }
+  },
+]
 
-profile_data = user_data.delete(:profile)
 
-user_email = user_data.delete(:email)
-user = User.find_or_initialize_by(email: user_email)
-user.assign_attributes(user_data)
-user.build_profile(profile_data)
-user.save!
+user_data.each do |args|
+  profile_data = args.delete(:profile)
 
-p user
+  email = args.delete(:email)
+  user = User.find_or_initialize_by(email: email)
+  user.assign_attributes(args)
+  user.build_profile(profile_data)
+  user.save!
+  p user
+end
+
 
 article_data = {
   title: 'テストタイトル',
   content: 'テストコンテンツ',
 }
+
+user = User.first
 
 article = user.articles.create(article_data)
 
